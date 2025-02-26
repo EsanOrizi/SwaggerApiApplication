722c743d6f61e4ef62d5caf0e552df784ceb7a1d
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2025-02-26 00:21:13 +0000

    Customising Swagger

--- SwaggerApiApp/Program.cs
+++ SwaggerApiApp/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SwaggerApiApp (This is out title)",
+        Version = "v1",
+        Description = "This is our description",
+        TermsOfService = new Uri("https://example.com/terms")
+    });
+});
 
 var app = builder.Build();
 
@@ -11,7 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger(opts =>
     {
-        opts.SerializeAsV2 = true;
+      //  opts.SerializeAsV2 = true;
     });
     app.UseSwaggerUI(opts =>
     {
